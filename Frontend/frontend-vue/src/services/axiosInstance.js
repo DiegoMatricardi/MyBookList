f06d363a5ada2c2be5612e083343fde1ceeef23d
Diego Matricardi <diegobmatricardi@hotmail.com>
2025-11-17 19:28:11 -0300

    Ajuste final

--- Frontend/frontend-vue/src/services/axiosInstance.js
+++ Frontend/frontend-vue/src/services/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: "http://localhost:3000",
-    timeout: 15000,
+    timeout: 30000,
     headers: {
         "Content-Type": "application/json"
     }
